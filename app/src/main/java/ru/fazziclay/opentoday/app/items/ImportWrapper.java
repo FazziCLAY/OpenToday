a5e0c1674d4df25f7aa5bae08a31c397a42a282b
fazziclay <fazziclay@gmail.com>
2022-10-28 15:23:24 +0300

    ! Version 0 -> 1: use gZip to compress

--- app/src/main/java/ru/fazziclay/opentoday/app/items/ImportWrapper.java
+++ app/src/main/java/ru/fazziclay/opentoday/app/items/ImportWrapper.java
@@ -2,17 +2,26 @@ package ru.fazziclay.opentoday.app.items;
 
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import ru.fazziclay.opentoday.app.items.item.Item;
 import ru.fazziclay.opentoday.app.items.item.ItemIEUtil;
 
 public class ImportWrapper {
-    public static final int VERSION = 0;
+    public static final int VERSION = 1;
 
     private final int importVersion = VERSION;
 
@@ -29,10 +38,13 @@ public class ImportWrapper {
 
         jsonObject.put("items", ItemIEUtil.exportItemList(items));
 
-        return "--OPENTODAY-IMPORT-START--\n" + importVersion + "\n" + Base64.getEncoder().encodeToString(jsonObject.toString().getBytes(StandardCharsets.UTF_8)) + "\n--OPENTODAY-IMPORT-END--";
+        String data = jsonObject.toString();
+        byte[] bytes = toGzip(data);
+        return "--OPENTODAY-IMPORT-START--\n" + importVersion + "\n" + Base64.getEncoder().encodeToString(bytes) + "\n--OPENTODAY-IMPORT-END--";
     }
 
     public static ImportWrapper finalImport(String content) throws Exception {
+        content = content.trim();
         if (!content.startsWith("--OPENTODAY-IMPORT-START--")) {
             throw new Exception("Error");
         }
@@ -41,19 +53,54 @@ public class ImportWrapper {
         }
 
         int version = Integer.parseInt(content.split("\n")[1]);
-        if (version != 0) throw new Exception("Version not compatible");
+        if (version != 0 && version != 1) throw new Exception("Version not compatible");
 
-        String data = new String(Base64.getDecoder().decode(content.split("\n")[2].getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
-        JSONObject jsonObject = new JSONObject(data);
+        byte[] bytes = Base64.getDecoder().decode(content.split("\n")[2].getBytes(StandardCharsets.UTF_8));
+        JSONObject jsonObject = null;
+        if (version == 0) {
+            jsonObject = new JSONObject(new String(bytes, StandardCharsets.UTF_8));
+        }
+        if (version == 1) {
+            String data = fromGzip(bytes);
+            jsonObject = new JSONObject(data);
+        }
 
         int importVersion = jsonObject.getInt("importVersion");
-        if (importVersion != 0) {
+        if (importVersion != 0 && importVersion != 1) {
             throw new Exception("Version not compatible");
         }
 
         return new ImportWrapper(ItemIEUtil.importItemList(jsonObject.getJSONArray("items")));
     }
 
+
+    private static byte[] toGzip(String s) throws IOException {
+        ByteArrayOutputStream oo = new ByteArrayOutputStream();
+        GZIPOutputStream o = new GZIPOutputStream(oo);
+        Writer writer = new OutputStreamWriter(o);
+        writer.write(s);
+        writer.flush();
+        writer.close();
+        return oo.toByteArray();
+    }
+
+    private static String fromGzip(byte[] s) throws IOException {
+        ByteArrayInputStream oo = new ByteArrayInputStream(s);
+        GZIPInputStream o = new GZIPInputStream(oo);
+        Reader reader = new InputStreamReader(o);
+
+        final StringBuilder result = new StringBuilder();
+
+        final char[] buff = new char[1024];
+        int i;
+        while ((i = reader.read(buff)) > 0) {
+            result.append(new String(buff, 0, i));
+        }
+
+        reader.close();
+
+        return result.toString();
+    }
 
     public List<Item> getItems() {
         return items;
