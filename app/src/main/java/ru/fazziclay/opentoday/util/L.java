30fb72d004570b2fd4ee0096c95d341477fcece1
fazziclay <fazziclay@gmail.com>
2022-10-28 15:14:07 +0300

    added ENABLED const

--- app/src/main/java/ru/fazziclay/opentoday/util/L.java
+++ app/src/main/java/ru/fazziclay/opentoday/util/L.java
@@ -2,10 +2,14 @@ package ru.fazziclay.opentoday.util;
 
 import android.util.Log;
 
+import java.util.Arrays;
+
+import ru.fazziclay.opentoday.app.App;
 import ru.fazziclay.opentoday.callback.CallbackStorage;
 import ru.fazziclay.opentoday.callback.Status;
 
 public class L {
+    public static final boolean ENABLED = (App.DEBUG && false);
     private static final L instance = new L();
 
     public static L getInstance() {
@@ -32,6 +36,10 @@ public class L {
     }
 
     public void o_(Object... objects) {
+        if (!ENABLED) {
+            Log.e("L", Arrays.toString(objects));
+            return;
+        }
         StringBuilder temp = new StringBuilder();
         String time = "";//System.currentTimeMillis() + "";
         temp.append("[").append(time).append("] ");
@@ -49,6 +57,6 @@ public class L {
     }
 
     public String getFinalText() {
-        return "=== LAST LOG ===\n" + text;
+        return ENABLED ? "=== LAST LOG ===\n" + text : "(Disabled)";
     }
 }
