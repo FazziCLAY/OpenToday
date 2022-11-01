34e2483d9e518c37c65f27e4178890a8ce5b5ebb
fazziclay <fazziclay@gmail.com>
2022-11-01 12:17:10 +0300

    add null checks & pretty & add final

--- app/src/main/java/ru/fazziclay/opentoday/ui/UI.java
+++ app/src/main/java/ru/fazziclay/opentoday/ui/UI.java
@@ -5,10 +5,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import ru.fazziclay.opentoday.ui.fragment.MainRootFragment;
+import ru.fazziclay.opentoday.ui.interfaces.NavigationHost;
 
 public class UI {
     @Nullable
-    public static Fragment findFragmentInParents(@NonNull Fragment fragment, Class<? extends Fragment> find) {
+    public static Fragment findFragmentInParents(@NonNull final Fragment fragment, @NonNull final Class<? extends Fragment> find) {
         if (fragment.getParentFragment() == null) {
             return null;
         }
@@ -19,8 +20,14 @@ public class UI {
         }
     }
 
-    public static void back(Fragment fragment) {
-        MainRootFragment host = (MainRootFragment) UI.findFragmentInParents(fragment, MainRootFragment.class);
+    public static void back(@NonNull final Fragment fragment) {
+        if (fragment == null) throw new NullPointerException("Fragment is null!");
+        final MainRootFragment host = (MainRootFragment) UI.findFragmentInParents(fragment, MainRootFragment.class);
+        if (host == null) throw new RuntimeException("fragment is not in MainRootFragment tree!");
         host.popBackStack();
+    }
+
+    public static void navigate(@NonNull final NavigationHost navigationHost, @NonNull final Fragment fragment, final boolean addToBackStack) {
+        navigationHost.navigate(fragment, addToBackStack);
     }
 }
