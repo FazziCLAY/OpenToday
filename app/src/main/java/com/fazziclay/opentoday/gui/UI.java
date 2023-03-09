c30e30a6115b53543aeeea693cca30ddaa894fe4
fazziclay <fazziclay@gmail.com>
2023-03-09 14:57:20 +0300

    findFragmentInParents use loop instead of recursion rewrite exception messages in UI.class

--- app/src/main/java/com/fazziclay/opentoday/gui/UI.java
+++ app/src/main/java/com/fazziclay/opentoday/gui/UI.java
@@ -11,22 +11,24 @@ public class UI {
     @Nullable
     public static <T extends Fragment> T findFragmentInParents(@NonNull final Fragment fragment, @NonNull final Class<T> find) {
         if (fragment == null) throw new NullPointerException("Fragment is null!");
-        if (find == null) throw new NullPointerException("find is null!");
-        if (fragment.getParentFragment() == null) {
-            return null;
-        }
+        if (find == null) throw new NullPointerException("Find is null!");
+
         Fragment parent = fragment.getParentFragment();
-        if (parent.getClass() == find) {
-            return (T) parent;
-        } else {
-            return findFragmentInParents(fragment.getParentFragment(), find);
+        while (true) {
+            if (parent == null) {
+                return null;
+            }
+            if (parent.getClass() == find) {
+                return (T) parent;
+            }
+            parent = parent.getParentFragment();
         }
     }
 
     public static void rootBack(@NonNull final Fragment fragment) {
         if (fragment == null) throw new NullPointerException("Fragment is null!");
         final MainRootFragment host = UI.findFragmentInParents(fragment, MainRootFragment.class);
-        if (host == null) throw new RuntimeException("fragment is not in MainRootFragment tree!");
+        if (host == null) throw new RuntimeException("Fragment is not contains MainRootFragment in parents!");
         host.popBackStack();
     }
 
