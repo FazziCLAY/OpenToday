f53e445d0222d9b9ab79232011003ff327e1dc12
fazziclay <fazziclay@gmail.com>
2023-03-10 10:56:35 +0300

    add getParentItemStorage to Item

--- app/src/main/java/com/fazziclay/opentoday/app/items/item/ItemController.java
+++ app/src/main/java/com/fazziclay/opentoday/app/items/item/ItemController.java
@@ -1,7 +1,10 @@
 package com.fazziclay.opentoday.app.items.item;
 
+import com.fazziclay.opentoday.app.items.ItemsStorage;
+
 public abstract class ItemController {
     public abstract void delete(Item item);
     public abstract void save(Item item);
     public abstract void updateUi(Item item);
+    public abstract ItemsStorage getParentItemStorage(Item item);
 }
