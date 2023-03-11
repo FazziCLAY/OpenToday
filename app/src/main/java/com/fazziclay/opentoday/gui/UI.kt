075a606413936b23d9025844f12b01551149fb61
fazziclay <fazziclay@gmail.com>
2023-03-11 12:28:15 +0300

    Merge gui.UI to kotlin

--- app/src/main/java/com/fazziclay/opentoday/gui/UI.kt
+++ app/src/main/java/com/fazziclay/opentoday/gui/UI.kt
@@ -1,138 +1,120 @@
-package com.fazziclay.opentoday.gui;
+package com.fazziclay.opentoday.gui
 
-import static com.fazziclay.opentoday.util.InlineUtil.viewClick;
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.widget.*
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.fazziclay.opentoday.R
+import com.fazziclay.opentoday.app.App
+import com.fazziclay.opentoday.app.FeatureFlag
+import com.fazziclay.opentoday.app.receiver.ItemsTickReceiver
+import com.fazziclay.opentoday.gui.fragment.MainRootFragment
+import com.fazziclay.opentoday.gui.interfaces.NavigationHost
+import com.fazziclay.opentoday.util.InlineUtil
+import java.util.*
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.fragment.app.Fragment;
-
-import com.fazziclay.opentoday.R;
-import com.fazziclay.opentoday.app.App;
-import com.fazziclay.opentoday.app.FeatureFlag;
-import com.fazziclay.opentoday.app.receiver.ItemsTickReceiver;
-import com.fazziclay.opentoday.gui.fragment.MainRootFragment;
-import com.fazziclay.opentoday.gui.interfaces.NavigationHost;
-
-import java.util.UUID;
-
-public class UI {
-    @Nullable
-    public static <T extends Fragment> T findFragmentInParents(@NonNull final Fragment fragment, @NonNull final Class<T> find) {
-        if (fragment == null) throw new NullPointerException("Fragment is null!");
-        if (find == null) throw new NullPointerException("Find is null!");
-
-        Fragment parent = fragment.getParentFragment();
+object UI {
+    @JvmStatic
+    fun <T : Fragment?> findFragmentInParents(fragment: Fragment, find: Class<T>): T? {
+        var parent = fragment.parentFragment
         while (true) {
             if (parent == null) {
-                return null;
+                return null
             }
-            if (parent.getClass() == find) {
-                return (T) parent;
+            if (parent.javaClass == find) {
+                return parent as T
             }
-            parent = parent.getParentFragment();
+            parent = parent.parentFragment
         }
     }
 
-    public static void rootBack(@NonNull final Fragment fragment) {
-        if (fragment == null) throw new NullPointerException("Fragment is null!");
-        final MainRootFragment host = UI.findFragmentInParents(fragment, MainRootFragment.class);
-        if (host == null) throw new RuntimeException("Fragment is not contains MainRootFragment in parents!");
-        host.popBackStack();
+    @JvmStatic
+    fun rootBack(fragment: Fragment) {
+        val host = findFragmentInParents(fragment, MainRootFragment::class.java) ?: throw RuntimeException("Fragment is not contains MainRootFragment in parents!")
+        host.popBackStack()
     }
 
-    public static void navigate(@NonNull final NavigationHost navigationHost, @NonNull final Fragment fragment, final boolean addToBackStack) {
-        navigationHost.navigate(fragment, addToBackStack);
+    @JvmStatic
+    fun navigate(navigationHost: NavigationHost, fragment: Fragment, addToBackStack: Boolean) {
+        navigationHost.navigate(fragment, addToBackStack)
     }
 
-    public static void setTheme(int i) {
-        AppCompatDelegate.setDefaultNightMode(i);
+    @JvmStatic
+    fun setTheme(i: Int) {
+        AppCompatDelegate.setDefaultNightMode(i)
     }
 
-    public static class Debug {
-        public static void showPersonalTickDialog(Context context) {
-            EditText view = new EditText(context);
-            new AlertDialog.Builder(context)
+    object Debug {
+        @JvmStatic
+        fun showPersonalTickDialog(context: Context) {
+            val view = EditText(context)
+            AlertDialog.Builder(context)
                     .setView(view)
-                    .setPositiveButton("TICK", (dfsd, fdsg) -> {
+                    .setPositiveButton("TICK") { dfsd: DialogInterface?, fdsg: Int ->
                         // TODO: 3/10/23 review
                         try {
-                            UUID id = UUID.fromString(view.getText().toString());
-                            context.sendBroadcast(new Intent(context, ItemsTickReceiver.class).putExtra(ItemsTickReceiver.EXTRA_PERSONAL_TICK, new String[]{id.toString()}).putExtra("debugMessage", "Debug personal tick is work!"));
-                        } catch (Exception e) {
-                            Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show();
+                            val id = UUID.fromString(view.text.toString())
+                            context.sendBroadcast(Intent(context, ItemsTickReceiver::class.java).putExtra(ItemsTickReceiver.EXTRA_PERSONAL_TICK, arrayOf(id.toString())).putExtra("debugMessage", "Debug personal tick is work!"))
+                        } catch (e: Exception) {
+                            Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show()
                         }
-                    })
-                    .show();
+                    }
+                    .show()
         }
 
-        public static void showCrashWithMessageDialog(final Context context, final String exceptionMessagePattern) {
-            Toast.makeText(context, R.string.manuallyCrash_crash, Toast.LENGTH_SHORT).show();
-            EditText message = new EditText(context);
-            message.setHint(R.string.manuallyCrash_dialog_inputHint);
-            Dialog dialog = new AlertDialog.Builder(context)
+        fun showCrashWithMessageDialog(context: Context?, exceptionMessagePattern: String?) {
+            Toast.makeText(context, R.string.manuallyCrash_crash, Toast.LENGTH_SHORT).show()
+            val message = EditText(context)
+            message.setHint(R.string.manuallyCrash_dialog_inputHint)
+            val dialog: Dialog = AlertDialog.Builder(context)
                     .setTitle(R.string.manuallyCrash_dialog_title)
                     .setView(message)
                     .setMessage(R.string.manuallyCrash_dialog_message)
-                    .setPositiveButton(R.string.manuallyCrash_dialog_apply, (var1, var2) -> {
-                        throw new RuntimeException(String.format(exceptionMessagePattern, message.getText().toString()));
-                    })
+                    .setPositiveButton(R.string.manuallyCrash_dialog_apply) { var1: DialogInterface?, var2: Int -> throw RuntimeException(String.format(exceptionMessagePattern!!, message.text.toString())) }
                     .setNegativeButton(R.string.manuallyCrash_dialog_cancel, null)
-                    .create();
-            dialog.setCanceledOnTouchOutside(false);
-            dialog.show();
+                    .create()
+            dialog.setCanceledOnTouchOutside(false)
+            dialog.show()
         }
 
-        public static void showFeatureFlagsDialog(final App app, final Context context) {
-            LinearLayout view = new LinearLayout(context);
-            view.setOrientation(LinearLayout.VERTICAL);
-
-            for (FeatureFlag featureFlag : FeatureFlag.values()) {
-                CheckBox c = new CheckBox(context);
-                c.setText(featureFlag.name());
-                c.setChecked(app.isFeatureFlag(featureFlag));
-                viewClick(c, () -> {
-                    boolean is = c.isChecked();
-                    if (is) {
+        fun showFeatureFlagsDialog(app: App, context: Context?) {
+            val view = LinearLayout(context)
+            view.orientation = LinearLayout.VERTICAL
+            for (featureFlag in FeatureFlag.values()) {
+                val c = CheckBox(context)
+                c.text = featureFlag.name
+                c.isChecked = app.isFeatureFlag(featureFlag)
+                InlineUtil.viewClick(c, Runnable {
+                    val `is` = c.isChecked
+                    if (`is`) {
                         if (!app.isFeatureFlag(featureFlag)) {
-                            app.getFeatureFlags().add(featureFlag);
+                            app.featureFlags.add(featureFlag)
                         }
                     } else {
                         if (app.isFeatureFlag(featureFlag)) {
-                            app.getFeatureFlags().remove(featureFlag);
+                            app.featureFlags.remove(featureFlag)
                         }
                     }
-                });
-
-                TextView textView = new TextView(context);
-                textView.setText(featureFlag.getDescription());
-                textView.setTextSize(11);
-                textView.setPadding(60, 0, 0, 0);
-
-                view.addView(c);
-                view.addView(textView);
+                })
+                val textView = TextView(context)
+                textView.text = featureFlag.description
+                textView.textSize = 11f
+                textView.setPadding(60, 0, 0, 0)
+                view.addView(c)
+                view.addView(textView)
             }
-
-            ScrollView scrollView = new ScrollView(context);
-            scrollView.addView(view);
-
-            Dialog dialog = new AlertDialog.Builder(context)
+            val scrollView = ScrollView(context)
+            scrollView.addView(view)
+            val dialog: Dialog = AlertDialog.Builder(context)
                     .setView(scrollView)
                     .setTitle("DEBUG: FeatureFlags")
                     .setNegativeButton(R.string.abc_cancel, null)
-                    .create();
-            dialog.show();
+                    .create()
+            dialog.show()
         }
     }
 }
