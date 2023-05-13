ab9dc9ce6f782dfd2844ccc0365003729f01dae6
fazziclay <fazziclay@gmail.com>
2023-05-13 18:55:23 +0300

    Make recyclable

--- app/src/main/java/com/fazziclay/opentoday/app/items/item/filter/FitEquip.java
+++ app/src/main/java/com/fazziclay/opentoday/app/items/item/filter/FitEquip.java
@@ -3,13 +3,29 @@ package com.fazziclay.opentoday.app.items.item.filter;
 import java.util.GregorianCalendar;
 
 public class FitEquip {
-    private final GregorianCalendar calendar;
+    private GregorianCalendar calendar;
+    private int recycle = 0; // Debug only
+
+    public FitEquip(GregorianCalendar calendar) {
+        this.calendar = calendar;
+    }
+
+    public FitEquip() {
+        this.calendar = null;
+    }
+
+    public void recycle(GregorianCalendar calendar) {
+        this.calendar = calendar;
+        recycle++;
+    }
+
 
     public GregorianCalendar getGregorianCalendar() {
         return calendar;
     }
 
-    public FitEquip(GregorianCalendar calendar) {
-        this.calendar = calendar;
+
+    public int getRecycle() {
+        return recycle;
     }
 }
