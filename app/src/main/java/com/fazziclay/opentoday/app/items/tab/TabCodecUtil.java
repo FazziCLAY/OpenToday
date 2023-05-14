8ad18b26bf1bed8769ec1770445808368eb7da8e
fazziclay <fazziclay@gmail.com>
2023-05-14 11:44:36 +0300

    mark finals

--- app/src/main/java/com/fazziclay/opentoday/app/items/tab/TabCodecUtil.java
+++ app/src/main/java/com/fazziclay/opentoday/app/items/tab/TabCodecUtil.java
@@ -14,13 +14,13 @@ public class TabCodecUtil {
     private static final TabsRegistry TABS_REGISTRY = TabsRegistry.REGISTRY;
 
     @NonNull
-    public static CherryOrchard exportTabList(@NonNull List<Tab> tabs) {
+    public static CherryOrchard exportTabList(@NonNull final List<Tab> tabs) {
         return exportTabList(tabs.toArray(new Tab[0]));
     }
 
     @NonNull
-    public static CherryOrchard exportTabList(@NonNull Tab[] tabs) {
-        CherryOrchard ret = new CherryOrchard();
+    public static CherryOrchard exportTabList(@NonNull final Tab[] tabs) {
+        final CherryOrchard ret = new CherryOrchard();
         for (Tab tab : tabs) {
             ret.put(exportTab(tab));
         }
@@ -28,20 +28,20 @@ public class TabCodecUtil {
     }
 
     @NonNull
-    public static List<Tab> importTabList(@NonNull CherryOrchard orchard) {
+    public static List<Tab> importTabList(@NonNull final CherryOrchard orchard) {
         final List<Tab> ret = new ArrayList<>();
         orchard.forEachCherry(cherry -> ret.add(importTab(cherry)));
         return ret;
     }
 
     @NonNull
-    public static Tab importTab(@NonNull Cherry cherry) {
+    public static Tab importTab(@NonNull final Cherry cherry) {
         final AbstractTabCodec codec = TABS_REGISTRY.get(cherry.getString(KEY_TABTYPE)).getCodec();
         return codec.importTab(cherry, null);
     }
 
     @NonNull
-    public static Cherry exportTab(@NonNull Tab tab) {
+    public static Cherry exportTab(@NonNull final Tab tab) {
         final TabsRegistry.TabInfo tabInfo = TABS_REGISTRY.get(tab.getClass());
         final AbstractTabCodec codec = tabInfo.getCodec();
         return codec.exportTab(tab)
