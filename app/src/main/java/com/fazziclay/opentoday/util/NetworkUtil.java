0a8b65e3fab18bc59a6fb4edea35107048ec0bd5
fazziclay <fazziclay@gmail.com>
2023-05-25 21:03:10 +0300

    perfectly code

--- app/src/main/java/com/fazziclay/opentoday/util/NetworkUtil.java
+++ app/src/main/java/com/fazziclay/opentoday/util/NetworkUtil.java
@@ -13,34 +13,36 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /**
  * for network
  * **/
 public class NetworkUtil {
+    private static final String TAG = "NetworkUtil";
     public static final List<LogInterface> NETWORK_LISTENERS = new ArrayList<>();
 
     /**
      * @return text of site
      * **/
     public static String parseTextPage(String url) throws IOException {
+        // LOG START
         int logKey = 0;
-        if (NETWORK_LISTENERS.size() > 0) logKey = generateLogKey();
+        if (!NETWORK_LISTENERS.isEmpty()) logKey = generateLogKey();
         for (LogInterface logInterface : NETWORK_LISTENERS) {
             logInterface.parseTextPage(logKey, 0, url, null);
         }
+        // LOG END
 
-        StringBuilder result = new StringBuilder();
-        URL pageUrl = new URL(url);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(pageUrl.openStream()));
+        final StringBuilder result = new StringBuilder();
+        final URL pageUrl = new URL(url);
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(pageUrl.openStream()));
         String line;
         while ((line = bufferedReader.readLine()) != null) {
             result.append(line).append("\n");
         }
         bufferedReader.close();
 
-        String ret = result.substring(0, result.lastIndexOf("\n"));
+        final String ret = result.substring(0, result.lastIndexOf("\n"));
         for (LogInterface logInterface : NETWORK_LISTENERS) {
             logInterface.parseTextPage(logKey, 1, url, ret);
         }
@@ -49,9 +51,7 @@ public class NetworkUtil {
     }
 
     private static int generateLogKey() {
-        int logKey = new Random().nextInt();
-        if (logKey < 0) logKey *= -1;
-        return logKey;
+        return RandomUtil.nextIntPositive();
     }
     
     public static void openBrowser(Activity activity, String url) {
@@ -59,6 +59,7 @@ public class NetworkUtil {
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
             activity.startActivity(browserIntent);
         } catch (Exception e) {
+            Logger.e(TAG, "openBrowser", e);
             Toast.makeText(activity, R.string.abc_error_browserNotFound, Toast.LENGTH_LONG).show();
         }
     }
