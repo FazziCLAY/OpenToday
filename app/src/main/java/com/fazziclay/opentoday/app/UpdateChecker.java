5ee4b75cef4e8d6f4e2466bcf72932959c180800
fazziclay <fazziclay@gmail.com>
2023-05-26 19:26:22 +0300

    UpdateChecker uses Logger insteadof Log

--- app/src/main/java/com/fazziclay/opentoday/app/UpdateChecker.java
+++ app/src/main/java/com/fazziclay/opentoday/app/UpdateChecker.java
@@ -1,9 +1,9 @@
 package com.fazziclay.opentoday.app;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.fazziclay.javaneoutil.FileUtil;
+import com.fazziclay.opentoday.util.Logger;
 import com.fazziclay.opentoday.util.NetworkUtil;
 
 import org.json.JSONObject;
@@ -15,6 +15,7 @@ public class UpdateChecker {
     private static final String V2_URL_LATEST = "https://fazziclay.github.io/api/project_3/v2/latest.json";
 
     private static final long CACHE_TIMEOUT_MILLIS = 5 * 60 * 60 * 1000; // 5 hours
+    private static final String TAG = "UpdateChecker";
 
     public static void check(Context context, Result result) {
         new Thread(() -> {
@@ -25,37 +26,37 @@ public class UpdateChecker {
                     long latestCheck = Long.parseLong(FileUtil.getText(cacheFile));
                     if (CURRENT_TIME - latestCheck < CACHE_TIMEOUT_MILLIS) {
                         result.run(false, null);
-                        Log.d("UpdateChecker", "run. available=false (CACHED!!!)");
+                        Logger.d(TAG, "run. available=false (CACHED!!!)");
                         return;
                     }
                 } catch (Exception e) {
-                    Log.d("UpdateChecker", "cache exception", e);
+                    Logger.d(TAG, "cache exception", e);
                 }
             }
 
             try {
                 String stringLatestBuild = NetworkUtil.parseTextPage(V2_URL_LATEST_BUILD);
                 int latestBuild = Integer.parseInt(stringLatestBuild);
-                Log.d("UpdateChecker", "latestBuild (remote) = " + latestBuild);
+                Logger.d(TAG, "latestBuild (remote) = " + latestBuild);
 
                 if (App.VERSION_CODE < latestBuild) {
                     String latestJsonString = NetworkUtil.parseTextPage(V2_URL_LATEST);
                     JSONObject latestJson = new JSONObject(latestJsonString);
-                    Log.d("UpdateChecker", "latest.json (remote) = " + latestJsonString);
+                    Logger.d(TAG, "latest.json (remote) = " + latestJsonString);
 
 
                     String url = latestJson.getString("page_url");
-                    Log.d("UpdateChecker", "url (remote) = " + url);
+                    Logger.d(TAG, "url (remote) = " + url);
                     result.run(true, url);
-                    Log.d("UpdateChecker", "run. available=true");
+                    Logger.d(TAG, "run. available=true");
                 } else {
                     result.run(false, null);
-                    Log.d("UpdateChecker", "run. available=false");
+                    Logger.d(TAG, "run. available=false");
                     FileUtil.setText(cacheFile, String.valueOf(CURRENT_TIME));
                 }
 
             } catch (Exception e) {
-                Log.d("UpdateChecker", "check exception", e);
+                Logger.d(TAG, "check exception", e);
             }
         }).start();
     }
