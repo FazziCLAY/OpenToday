0dccaf033ff2ae8f2470c191634184a717774ec9
fazziclay <fazziclay@gmail.com>
2023-06-04 23:04:50 +0300

    add isInherit

--- app/src/main/java/com/fazziclay/opentoday/app/items/item/ItemType.java
+++ app/src/main/java/com/fazziclay/opentoday/app/items/item/ItemType.java
@@ -1,14 +1,29 @@
 package com.fazziclay.opentoday.app.items.item;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum ItemType {
-    DEBUG_TICK_COUNTER,
     TEXT,
-    LONG_TEXT,
-    CHECKBOX,
-    CHECKBOX_DAY_REPEATABLE,
-    COUNTER,
-    CYCLE_LIST,
-    GROUP,
-    FILTER_GROUP,
-    MATH_GAME
+    DEBUG_TICK_COUNTER(TEXT),
+    LONG_TEXT(TEXT),
+    CHECKBOX(TEXT),
+    CHECKBOX_DAY_REPEATABLE(TEXT, CHECKBOX),
+    COUNTER(TEXT),
+    CYCLE_LIST(TEXT),
+    GROUP(TEXT),
+    FILTER_GROUP(TEXT),
+    MATH_GAME(TEXT);
+
+    private final List<ItemType> parents = new ArrayList<>();
+
+    ItemType(ItemType... parents) {
+        this.parents.addAll(Arrays.asList(parents));
+    }
+
+    public boolean isInherit(ItemType type) {
+        if (type == this) return true;
+        return this.parents.contains(type);
+    }
 }
