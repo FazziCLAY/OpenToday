9d646d41381345ba035b295776ab25a7e637d96d
fazziclay <fazziclay@gmail.com>
2023-06-04 12:10:02 +0300

    add ItemsRoot

--- app/src/main/java/com/fazziclay/opentoday/app/items/ItemsRoot.java
+++ app/src/main/java/com/fazziclay/opentoday/app/items/ItemsRoot.java
@@ -1,4 +1,31 @@
 package com.fazziclay.opentoday.app.items;
 
+import com.fazziclay.opentoday.app.items.item.Item;
+import com.fazziclay.opentoday.app.items.tab.Tab;
+
+import java.util.UUID;
+
 public interface ItemsRoot {
+    /**
+     * Getting item by ItemID
+     * @param id id
+     * @return item is exist, is not exist: null
+     */
+    Item getItemById(UUID id);
+
+    /**
+     * Getting tab by TabID
+     * @param id id
+     * @return tab is exist, is not exist: null
+     */
+    Tab getTabById(UUID id);
+
+    boolean isExistById(UUID id);
+
+    Type getTypeById(UUID id);
+
+    enum Type {
+        TAB,
+        ITEM
+    }
 }
