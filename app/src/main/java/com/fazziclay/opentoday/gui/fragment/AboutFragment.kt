a3df1a93b715d248c52f1d527b3a7c8ee1297180
fazziclay <fazziclay@gmail.com>
2023-06-05 19:59:36 +0300

    use ActivitySettings by AboutFragment remove title TextItem (now in Toolbar) move releaseDate to external TextView

--- app/src/main/java/com/fazziclay/opentoday/gui/fragment/AboutFragment.kt
+++ app/src/main/java/com/fazziclay/opentoday/gui/fragment/AboutFragment.kt
@@ -9,13 +9,15 @@ import androidx.fragment.app.Fragment
 import com.fazziclay.opentoday.R
 import com.fazziclay.opentoday.app.App
 import com.fazziclay.opentoday.databinding.FragmentAboutBinding
+import com.fazziclay.opentoday.gui.ActivitySettings
 import com.fazziclay.opentoday.gui.UI
 import com.fazziclay.opentoday.gui.activity.ChangelogActivity
 import com.fazziclay.opentoday.gui.activity.OpenSourceLicensesActivity
 import com.fazziclay.opentoday.util.InlineUtil.viewClick
 import com.fazziclay.opentoday.util.NetworkUtil
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
 
 class AboutFragment : Fragment() {
     companion object {
@@ -31,11 +33,26 @@ class AboutFragment : Fragment() {
     private var easterEggLastClick: Long = 0
     private var easterEggCounter = 0
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        UI.getUIRoot(this).pushActivitySettings { a ->
+            a.isClockVisible = false
+            a.isNotificationsVisible = false
+            a.toolbarSettings = ActivitySettings.ToolbarSettings.createBack(R.string.aboutapp_title) { UI.rootBack(this) }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        UI.getUIRoot(this).popActivitySettings()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentAboutBinding.inflate(inflater)
-        binding.textVersion.text = App.VERSION_NAME + " " + getReleaseTime()
+        binding.textVersion.text = App.VERSION_NAME
+        binding.textReleaseTime.text = getReleaseTime()
         binding.textPackage.text = App.APPLICATION_ID
-        viewClick(binding.title, this::manuallyCrashInteract)
+        viewClick(binding.aboutText, this::manuallyCrashInteract)
         viewClick(binding.sourceCode, Runnable { NetworkUtil.openBrowser(requireActivity(), LINK_OPENSOURCE) })
         viewClick(binding.issues, Runnable { NetworkUtil.openBrowser(requireActivity(), LINK_ISSUES) })
         viewClick(binding.licenses, Runnable { requireActivity().startActivity(OpenSourceLicensesActivity.createLaunchIntent(requireContext())) })
@@ -45,7 +62,7 @@ class AboutFragment : Fragment() {
     }
 
     private fun getReleaseTime(): String? {
-        return SimpleDateFormat("yyyy.MM.dd HH:mm:ss").format(Date(App.VERSION_RELEASE_TIME * 1000))
+        return SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.getDefault()).format(Date(App.VERSION_RELEASE_TIME * 1000))
     }
 
     private fun manuallyCrashInteract() {
