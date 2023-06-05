c5d6c74dd383d941e3a25e501e064ae5d7d1c18a
fazziclay <fazziclay@gmail.com>
2023-06-05 22:14:34 +0300

    no delay if App.DEBUG == false

--- app/src/main/java/com/fazziclay/opentoday/util/DebugUtil.java
+++ app/src/main/java/com/fazziclay/opentoday/util/DebugUtil.java
@@ -1,7 +1,10 @@
 package com.fazziclay.opentoday.util;
 
+import com.fazziclay.opentoday.app.App;
+
 public class DebugUtil {
     public static void sleep(int millis) {
+        if (!App.DEBUG) return;
         if (millis > 0) {
             try {
                 Thread.sleep(millis);
