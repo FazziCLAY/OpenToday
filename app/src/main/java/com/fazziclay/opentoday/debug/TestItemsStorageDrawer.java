8cb31efe4ecffdf589e7215ff1a92985f9af4b8f
fazziclay <fazziclay@gmail.com>
2023-06-06 12:48:25 +0300

    debug

--- app/src/main/java/com/fazziclay/opentoday/debug/TestItemsStorageDrawer.java
+++ app/src/main/java/com/fazziclay/opentoday/debug/TestItemsStorageDrawer.java
@@ -12,14 +12,13 @@ import androidx.annotation.Nullable;
 
 import com.fazziclay.opentoday.app.SettingsManager;
 import com.fazziclay.opentoday.app.Translation;
-import com.fazziclay.opentoday.app.items.item.Item;
-import com.fazziclay.opentoday.app.items.selection.SelectionManager;
-import com.fazziclay.opentoday.app.items.tab.TabsManager;
 import com.fazziclay.opentoday.app.items.item.CycleListItem;
 import com.fazziclay.opentoday.app.items.item.FilterGroupItem;
 import com.fazziclay.opentoday.app.items.item.GroupItem;
+import com.fazziclay.opentoday.app.items.item.Item;
+import com.fazziclay.opentoday.app.items.selection.SelectionManager;
+import com.fazziclay.opentoday.app.items.tab.TabsManager;
 import com.fazziclay.opentoday.gui.interfaces.ItemInterface;
-import com.fazziclay.opentoday.gui.interfaces.StorageEditsActions;
 import com.fazziclay.opentoday.gui.item.ItemViewGeneratorBehavior;
 import com.fazziclay.opentoday.gui.item.ItemsStorageDrawer;
 
@@ -39,22 +38,6 @@ public class TestItemsStorageDrawer extends Activity {
         });
         ItemInterface onClick = item -> Toast.makeText(this, "item = " + item.toString(), Toast.LENGTH_SHORT).show();
         boolean previewMode = false;
-        StorageEditsActions edits = new StorageEditsActions() {
-            @Override
-            public void onGroupEdit(GroupItem groupItem) {
-                Toast.makeText(TestItemsStorageDrawer.this, "Edit: Group: " + groupItem.toString(), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCycleListEdit(CycleListItem cycleListItem) {
-                Toast.makeText(TestItemsStorageDrawer.this, "Edit: CycleList: " + cycleListItem.toString(), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onFilterGroupEdit(FilterGroupItem filterGroupItem) {
-                Toast.makeText(TestItemsStorageDrawer.this, "Edit: FilterGroup: " + filterGroupItem.toString(), Toast.LENGTH_SHORT).show();
-            }
-        };
 
         ItemsStorageDrawer itemsStorageDrawer = new ItemsStorageDrawer(this,
                 new ItemViewGeneratorBehavior() {
@@ -95,17 +78,17 @@ public class TestItemsStorageDrawer extends Activity {
 
                     @Override
                     public void onGroupEdit(GroupItem groupItem) {
-
+                        Toast.makeText(TestItemsStorageDrawer.this, "Edit: Group: " + groupItem.toString(), Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onCycleListEdit(CycleListItem cycleListItem) {
-
+                        Toast.makeText(TestItemsStorageDrawer.this, "Edit: CycleList: " + cycleListItem.toString(), Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onFilterGroupEdit(FilterGroupItem filterGroupItem) {
-
+                        Toast.makeText(TestItemsStorageDrawer.this, "Edit: FilterGroup: " + filterGroupItem.toString(), Toast.LENGTH_SHORT).show();
                     }
                 },
                 new SelectionManager(),
