dd67e7576bc0c867b496da5d9d0da6c7b64c9a6d
fazziclay <fazziclay@gmail.com>
2023-06-06 12:20:53 +0300

    add ITEM_MATH_GAME_UPDATE TickTarget to important

--- app/src/main/java/com/fazziclay/opentoday/app/items/item/ItemUtil.java
+++ app/src/main/java/com/fazziclay/opentoday/app/items/item/ItemUtil.java
@@ -108,7 +108,8 @@ public class ItemUtil {
             TickTarget.ITEM_FILTER_GROUP_TICK,
             TickTarget.ITEM_DAY_REPEATABLE_CHECKBOX_UPDATE,
             TickTarget.ITEM_NOTIFICATIONS,
-            TickTarget.ITEM_NOTIFICATION_SCHEDULE);
+            TickTarget.ITEM_NOTIFICATION_SCHEDULE,
+            TickTarget.ITEM_MATH_GAME_UPDATE);
     public static void tickOnlyImportantTargets(TickSession tickSession, Item[] items) {
         // NOTE: No use 'for-loop' (self-delete item in tick => ConcurrentModificationException)
         int i = items.length - 1;
