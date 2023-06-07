2b2d9045fa66af505205be2e3ddbc00bf4cbbf04
fazziclay <fazziclay@gmail.com>
2023-06-07 11:03:56 +0300

    rename activity_changelog.xml to fragment_changelog.xml

--- app/src/main/java/com/fazziclay/opentoday/gui/fragment/ChangelogFragment.kt
+++ app/src/main/java/com/fazziclay/opentoday/gui/fragment/ChangelogFragment.kt
@@ -9,7 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.fazziclay.opentoday.R
-import com.fazziclay.opentoday.databinding.ActivityChangelogBinding
+import com.fazziclay.opentoday.databinding.FragmentChangelogBinding
 import com.fazziclay.opentoday.gui.ActivitySettings
 import com.fazziclay.opentoday.gui.UI
 import com.fazziclay.opentoday.util.ColorUtil
@@ -26,7 +26,8 @@ class ChangelogFragment : Fragment() {
         private const val TAG: String = "ChangelogFragment"
     }
 
-    private lateinit var binding: ActivityChangelogBinding
+    private lateinit var binding: FragmentChangelogBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +50,7 @@ class ChangelogFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = ActivityChangelogBinding.inflate(layoutInflater)
+        binding = FragmentChangelogBinding.inflate(layoutInflater)
         try {
             binding.changelogText.text = ColorUtil.colorize(StreamUtil.read(requireActivity().assets.open("CHANGELOG")), ResUtil.getAttrColor(requireContext(), com.google.android.material.R.attr.colorOnBackground), Color.TRANSPARENT, Typeface.NORMAL)
         } catch (e: Exception) {
