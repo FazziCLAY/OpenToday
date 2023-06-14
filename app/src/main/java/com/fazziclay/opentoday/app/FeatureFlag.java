41efe1fd86903a7fab8c36b427719b02e65fdb59
fazziclay <fazziclay@gmail.com>
2023-06-14 13:15:08 +0300

    fix ancient names

--- app/src/main/java/com/fazziclay/opentoday/app/FeatureFlag.java
+++ app/src/main/java/com/fazziclay/opentoday/app/FeatureFlag.java
@@ -2,7 +2,7 @@ package com.fazziclay.opentoday.app;
 
 public enum FeatureFlag {
     ITEM_DEBUG_TICK_COUNTER("Show hidden debug item: DebugTickCounter"),
-    ALWAYS_SHOW_SAVE_STATUS("Always show save status on ItemManager using Toast"),
+    ALWAYS_SHOW_SAVE_STATUS("Always show save status on TabsManager using Toast"),
     DISABLE_AUTOMATIC_TICK("Disable auto-tick calling by MainActivity"),
     DISABLE_DEBUG_MODE_NOTIFICATION("Disable debug-app-warning in MainActivity"),
     TOOLBAR_DEBUG("Show hidden DEBUG section in Toolbar");
