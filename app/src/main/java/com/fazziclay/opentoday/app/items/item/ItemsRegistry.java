1a03eebee962f6d8011ce3e38500ac35280c4b32
fazziclay <fazziclay@gmail.com>
2023-09-21 22:17:14 +0300

    fix ItemsRegistry

--- app/src/main/java/com/fazziclay/opentoday/app/items/item/ItemsRegistry.java
+++ app/src/main/java/com/fazziclay/opentoday/app/items/item/ItemsRegistry.java
@@ -33,7 +33,7 @@ public class ItemsRegistry {
             new ItemInfo(GroupItem.class,                          "GroupItem",                     ItemType.GROUP,                      GroupItem.CODEC,                      GroupItem::createEmpty,                   (i) -> new GroupItem((GroupItem) i)),
             new ItemInfo(FilterGroupItem.class,                    "FilterGroupItem",               ItemType.FILTER_GROUP,               FilterGroupItem.CODEC,                FilterGroupItem::createEmpty,             (i) -> new FilterGroupItem((FilterGroupItem) i)),
             new ItemInfo(MathGameItem.class,                       "MathGameItem",                  ItemType.MATH_GAME,                  MathGameItem.CODEC,                   MathGameItem::createEmpty,                (i) -> new MathGameItem((MathGameItem) i)),
-            new ItemInfo(SleepTimeItem.class,                      "SleepTimeItem",                 ItemType.SLEEP_TIME,                 SleepTimeItem.CODEC,                  SleepTimeItem::createEmpty,               (i) -> new SleepTimeItem((SleepTimeItem) i)).requiredFeatureFlag(FeatureFlag.ITEM_SLEEP_TIME)
+            new ItemInfo(SleepTimeItem.class,                      "SleepTimeItem",                 ItemType.SLEEP_TIME,                 SleepTimeItem.CODEC,                  SleepTimeItem::createEmpty,               (i) -> new SleepTimeItem((SleepTimeItem) i)).requiredFeatureFlag(FeatureFlag.ITEM_SLEEP_TIME),
             new ItemInfo(MissingNoItem.class,                      "MissingNoItem",                 ItemType.MISSING_NO,                 MissingNoItem.CODEC,                  TextItem::createEmpty,                    (i) -> new TextItem("'missing no' item not support copy :(")).noAvailableToCreate(),
     };
 
